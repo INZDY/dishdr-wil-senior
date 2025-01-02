8418f1beca87f5263b8e33ded4d98744b84baa6b
Aphivit <aphivit12507.jira@gmail.com>
2025-01-02 21:41:07 +0700

    chore: removal of existing vite frontend for a full framework setup

--- client/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
-        <p>© 2025 Integrace Solution. All rights reserved.</p>
-      </footer>
-  )
-}
