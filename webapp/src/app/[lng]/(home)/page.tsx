5bc1cf508f4520d8cd57af3e67dfd4f73ee6dcaa
Aphivit <aphivit12507.jira@gmail.com>
2025-03-17 09:09:55 +0700

    style: login page

--- webapp/src/app/[lng]/(home)/page.tsx
+++ webapp/src/app/[lng]/(home)/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button"; // Assuming you have a Button c
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useTranslation } from "@/app/i18n/client";
+import Image from "next/image";
 
 export default function Auth({ params }: { params: any }) {
   const { lng } = React.use<{ lng: string }>(params);
@@ -16,14 +17,13 @@ export default function Auth({ params }: { params: any }) {
   }
 
   return (
-    <div className="flex flex-col items-center max-w-screen-md mx-auto my-12 py-36 gap-4 bg-white shadow rounded">
+    <div className="flex flex-col items-center max-w-screen-md mx-auto my-12 pt-24 pb-36 gap-2 bg-neutral-100 shadow rounded">
       <h1 className="text-2xl font-bold mb-4">{t("login")}</h1>
       <Button
-        className="flex px-8 bg-green-500 text-black"
+        className="flex px-8 py-6 bg-green-500"
         onClick={() => signIn("line", { redirectTo: `/${lng}/activity` })}
       >
-        {/* <div>LINELOGO</div> */}
-        {/* <Image src={`/line_44.png`} alt="icon" width={40} height={40}/> */}
+        {/* <Image src="/line_44.png" alt="icon" width={20} height={20} /> */}
         <p className="text-white font-bold">{t("line-login")}</p>
       </Button>
     </div>
