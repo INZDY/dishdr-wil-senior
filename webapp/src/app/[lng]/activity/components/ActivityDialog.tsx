7ce0c9d312ee98a4052a7e7680e0643b0db58835
Aphivit <aphivit12507.jira@gmail.com>
2025-05-17 21:33:51 +0700

    fix: placeholder value and grid layout in Activity Dialog, Question language in symptom inquiry.

--- webapp/src/app/[lng]/activity/components/ActivityDialog.tsx
+++ webapp/src/app/[lng]/activity/components/ActivityDialog.tsx
@@ -46,6 +46,16 @@ export default function ActivityDialog({
   const { t } = useTranslation(lng, "activity");
   const original = selectedAppointment;
 
+  const symptomTypeExist = (type: string) => {
+    return selectedAppointment.symptoms.some((s) => s.type === type);
+  };
+
+  const staffRolePredictionCheck = () => {
+    return (
+      currentUser?.role === "staff" && !!selectedAppointment.prediction?.length
+    );
+  };
+
   return (
     <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
       <DialogContent
@@ -57,49 +67,49 @@ export default function ActivityDialog({
         </DialogHeader>
 
         {selectedAppointment !== undefined && (
-          <div className="grid gap-4 py-2">
-            <p>
-              <span className="font-semibold mr-2">{t("name")}:</span>
-              {selectedAppointment.name}
+          <div className="grid grid-cols-5 gap-4 py-2">
+            <p className="col-span-2 font-semibold">{t("name")}:</p>
+            <p className="col-span-3">{selectedAppointment.name}</p>
+            <p className="col-span-2 font-semibold">{t("hn")}:</p>
+            <p className="col-span-3">
+              {!!selectedAppointment.hn?.length ? selectedAppointment.hn : "-"}
             </p>
-            <p>
-              <span className="font-semibold mr-2">{t("hn")}:</span>
-              {selectedAppointment.hn}
+            <p className="col-span-2 font-semibold">{t("phone")}:</p>
+            <p className="col-span-3">{selectedAppointment.phone}</p>
+            <p className="col-span-2 font-semibold">{t("chief")}:</p>
+            <p className="col-span-3 flex">
+              {symptomTypeExist("chief")
+                ? selectedAppointment.symptoms.map((s, index) => {
+                    if (s.type === "chief") {
+                      return (
+                        <span key={index}>
+                          {lng === "th" ? s.symptomTh : s.symptom}, {s.duration}{" "}
+                          {t(`${s.unit}`)}
+                        </span>
+                      );
+                    }
+                  })
+                : "-"}
             </p>
-            <p>
-              <span className="font-semibold mr-2">{t("phone")}:</span>
-              {selectedAppointment.phone}
-            </p>
-            <p className="flex">
-              <span className="font-semibold mr-2">{t("chief")}:</span>
-              {selectedAppointment.symptoms.map((s, index) => {
-                if (s.type === "chief") {
-                  return (
-                    <span key={index}>
-                      {lng === "th" ? s.symptomTh : s.symptom}, {s.duration}{" "}
-                      {t(`${s.unit}`)}
-                    </span>
-                  );
-                }
-              })}
-            </p>
-            <div className="flex">
-              <span className="font-semibold mr-2">{t("illness")}:</span>
+            <p className="col-span-2 font-semibold">{t("illness")}:</p>
+            <div className="col-span-3 flex">
               <div className="flex flex-col gap-1">
-                {selectedAppointment.symptoms.map((s, index) => {
-                  if (s.type === "present") {
-                    return (
-                      <p key={index}>
-                        {lng === "th" ? s.symptomTh : s.symptom}, {s.duration}{" "}
-                        {t(`${s.unit}`)}
-                      </p>
-                    );
-                  }
-                })}
+                {symptomTypeExist("present")
+                  ? selectedAppointment.symptoms.map((s, index) => {
+                      if (s.type === "present") {
+                        return (
+                          <p key={index}>
+                            {lng === "th" ? s.symptomTh : s.symptom},{" "}
+                            {s.duration} {t(`${s.unit}`)}
+                          </p>
+                        );
+                      }
+                    })
+                  : "-"}
               </div>
             </div>
-            <div>
-              <span className="font-semibold mr-2">{t("app-date")}:</span>
+            <p className="col-span-2 font-semibold">{t("app-date")}:</p>
+            <div className="col-span-3">
               {currentUser?.role === "patient" ? (
                 <span>
                   {format(
@@ -111,7 +121,7 @@ export default function ActivityDialog({
                 <div className="flex gap-2">
                   <input
                     type="date"
-                    className="px-2 py-1 rounded-md"
+                    className="p-1 rounded-md"
                     defaultValue={format(
                       new Date(selectedAppointment.dateTime),
                       "yyyy-MM-dd"
@@ -138,7 +148,7 @@ export default function ActivityDialog({
                   />
                   <input
                     type="time"
-                    className="px-2 py-1 rounded-md"
+                    className="p-1 rounded-md"
                     defaultValue={format(
                       new Date(selectedAppointment.dateTime),
                       "HH:mm"
@@ -159,8 +169,8 @@ export default function ActivityDialog({
                 </div>
               )}
             </div>
-            <p>
-              <span className="font-semibold mr-2">{t("dept")}:</span>
+            <p className="col-span-2 font-semibold">{t("dept")}:</p>
+            <p className="col-span-3">
               {currentUser?.role === "patient" ? (
                 <span>
                   {!!selectedAppointment.department.length
@@ -200,8 +210,8 @@ export default function ActivityDialog({
                 </Select>
               )}
             </p>
-            <p>
-              <span className="font-semibold mr-2">{t("status")}:</span>
+            <p className="col-span-2 font-semibold">{t("status")}:</p>
+            <p className="col-span-3">
               {currentUser?.role === "patient" ? (
                 <span>{t(`${selectedAppointment.status}`)}</span>
               ) : (
@@ -230,57 +240,58 @@ export default function ActivityDialog({
               )}
             </p>
 
-            <Separator />
+            <Separator className="col-span-5" />
             {/* New section for inquiry symptoms */}
-            {currentUser?.role === "staff" &&
-              !!selectedAppointment.prediction?.length && (
-                <div className="flex flex-col mt-2 gap-2">
-                  <h3 className="font-semibold text-lg mb-2">
-                    {t("inquiry-details")}
-                  </h3>
-                  <div className="flex flex-col gap-2">
-                    {selectedAppointment.symptoms.map((s, index) => {
-                      if (s.type === "inquiry") {
-                        return (
-                          <div
-                            key={index}
-                            className="p-2 border rounded-md bg-gray-50"
-                          >
-                            <p>
-                              <span className="font-semibold mr-2">
-                                {t("question")}:
-                              </span>
-                              {lng === "th" ? s.symptomTh : s.symptom}
-                            </p>
-                            <p>
-                              <span className="font-semibold mr-2">
-                                {t("answer")}:
-                              </span>
-                              {s.hasSymptom ? t("yes") : t("no")}
-                            </p>
-                          </div>
-                        );
-                      }
-                    })}
-                  </div>
-                  <div className="flex">
-                    <span className="font-semibold text-base mr-2">
-                      {t("predicted-dept")}:
-                    </span>
-                    <p>
-                      {lng === "th"
-                        ? selectedAppointment.predictionTh
-                        : selectedAppointment.prediction}
-                    </p>
-                  </div>
+            {staffRolePredictionCheck() && (
+              <div className="col-span-5 flex flex-col mt-2 gap-4">
+                <h3 className="font-semibold text-lg">
+                  {t("inquiry-details")}
+                </h3>
+                <div className="flex flex-col gap-2">
+                  {selectedAppointment.symptoms.map((s, index) => {
+                    if (s.type === "inquiry") {
+                      return (
+                        <div
+                          key={index}
+                          className="p-2 grid grid-cols-5 border rounded-md bg-gray-50"
+                        >
+                          <p className="col-span-1 font-semibold">
+                            {t("question")}:
+                          </p>
+                          <p className="col-span-4">
+                            {lng === "th" ? s.symptomTh : s.symptom}
+                          </p>
+                          <p className="col-span-1 font-semibold">
+                            {t("answer")}:
+                          </p>
+                          <p className="col-span-4">
+                            {s.hasSymptom ? t("yes") : t("no")}
+                          </p>
+                        </div>
+                      );
+                    }
+                  })}
                 </div>
-              )}
+                <div className="flex">
+                  <span className="mr-2 font-semibold text-base">
+                    {t("predicted-dept")}:
+                  </span>
+                  <p>
+                    {lng === "th"
+                      ? selectedAppointment.predictionTh
+                      : selectedAppointment.prediction}
+                  </p>
+                </div>
+                <Separator className="col-span-5" />
+              </div>
+            )}
           </div>
         )}
         <DialogFooter>
-          <div className="flex gap-2 justify-end">
+          <div className="flex gap-6 justify-end">
             {currentUser?.role === "staff" && (
               <Button
+                className="bg-green-600 hover:bg-green-700 text-white"
                 onClick={() => {
                   handleEdit(selectedAppointment, original);
                   setDialogOpen(false);
