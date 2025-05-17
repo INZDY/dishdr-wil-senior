259ff871e370be858d849695d1886abae1a4aae2
Aphivit <aphivit12507.jira@gmail.com>
2025-05-17 21:49:10 +0700

    fix: line logo

--- webapp/src/app/[lng]/(login)/page.tsx
+++ webapp/src/app/[lng]/(login)/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button"; // Assuming you have a Button component
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useTranslation } from "@/app/i18n/client";
 import Image from "next/image";
+import line from "@/../public/line.png";
 
 export default function Auth({ params }: { params: any }) {
   const { lng } = React.use<{ lng: string }>(params);
@@ -20,10 +21,10 @@ export default function Auth({ params }: { params: any }) {
     <div className="flex flex-col items-center max-w-screen-md mx-auto my-12 pt-24 pb-36 gap-2 bg-neutral-100 shadow rounded">
       <h1 className="text-2xl font-bold mb-4">{t("login")}</h1>
       <Button
-        className="flex px-8 py-6 bg-green-500"
+        className="flex p-6 bg-green-500"
         onClick={() => signIn("line", { redirectTo: `/${lng}/activity` })}
       >
-        {/* <Image src="/line_44.png" alt="icon" width={20} height={20} /> */}
+        <Image src={line} alt="" width={35} height={35} />
         <p className="text-white font-bold">{t("line-login")}</p>
       </Button>
     </div>
